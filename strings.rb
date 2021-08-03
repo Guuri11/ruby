c787ab5609ac2b3d3efaf34c067cad8306588332
Guuri11 <sergio.gurillo11@gmail.com>
2021-08-03 23:19:26 +0200

    playing w/ strings

---
+++ strings.rb
@@ -0,0 +1,15 @@
+phrase = "  \"Ruby\" rocks \n React rocks too  "
+
+#puts phrase.upcase()
+
+#puts phrase.downcase()
+
+#puts phrase.strip()
+
+#puts phrase.include? "React" => expects true 
+
+#puts phrase[3] => expects R
+
+#puts phrase.index(R) => expects 3
+
+# "string".index(s) => 0
